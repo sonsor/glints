e4a7c520ce090305b107dab06c8f1ee976358c3f
wasif Farooq <wasif0332@gmail.com>
2022-07-02 10:46:12 +0500

    feat: configure env variables

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import Configuration from './config/configuration';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,6 @@ async function bootstrap() {
   SwaggerModule.setup('documentation', app, document);
 
 
-  await app.listen(3000);
+  await app.listen(Configuration().port);
 }
 bootstrap();
