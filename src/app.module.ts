119f32b15f9eb73d2d9349b7cbd51dcf670523f5
wasif Farooq <wasif0332@gmail.com>
2022-07-02 12:22:10 +0500

    feat: setup database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import configuration from './config/configuration';
 import {ConfigModule} from "@nestjs/config";
+import { DatabaseService } from './database/database.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 
 @Module({
   imports: [
       ConfigModule.forRoot({
         load: [configuration],
+      }),
+      TypeOrmModule.forRootAsync({
+          imports: [ConfigModule],
+          useClass: DatabaseService,
       })
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DatabaseService],
 })
 export class AppModule {}
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -3,7 +3,7 @@ export default () => ({
     database: {
         host: process.env.DATABASE_HOST,
         port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-        user: process.env.DATABASE_USER || '',
+        username: process.env.DATABASE_USER || '',
         password: process.env.DATABASE_PASSWORD || '',
     }
 });
